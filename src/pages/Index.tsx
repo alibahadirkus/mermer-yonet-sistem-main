130e000b2cf3b5bcc08cabb1ca3c3343cf5dafb1
alibahadirkus <alibahadirkus@gmail.com>
2025-09-16 23:10:11 +0300

    a

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -22,7 +22,7 @@ const Index = () => {
             className="w-full h-full object-cover"
             poster="https://images.unsplash.com/photo-1589833870464-a77ee1183f65?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80"
           >
-            <source src="/src/image/video.mp4" type="video/mp4" />
+            <source src="/videos/video.mp4" type="video/mp4" />
             Tarayıcınız video etiketini desteklemiyor.
           </video>
           <div className="absolute inset-0 bg-black/50"></div>
